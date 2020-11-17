ad415364b885450108f3f51ce19607690b3ad33a
TimothyRay18 <timothy.ray1802@gmail.com>
2020-11-17 20:18:05 +0700

    tambah libraries

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,7 +1,13 @@
 package Main;
 
+import Controller.ControllerDatabase;
+
+import java.sql.SQLOutput;
+
 public class Main {
     public static void main(String[] args) {
-
+//        for(int i=0;i<ControllerDatabase.getAllUsernames().size();i++){
+//            System.out.println(ControllerDatabase.getAllUsernames().get(i));
+//        }
     }
 }
